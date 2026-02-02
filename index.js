79b2d21cce4326ecfe208a543dff3573e1423eb0
Surya Prakash <suryapr1237@gmail.com>
2026-02-02 21:47:26 +0530

    updated index.js

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.get("/", (req, res) => {
 // listen always at the end
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
